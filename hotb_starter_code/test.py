fbae908ba355bed73bc101d9ba73eabe1d91ad00
Jan Rudnicki <s203179@student.pg.edu.pl>
2025-11-29 15:11:13 +0100

    changed the test,py script

--- hotb_starter_code/test.py
+++ hotb_starter_code/test.py
@@ -1,15 +1,9 @@
-""" EEG measurement example
 
-Example how to get measurements and
-save to fif format
-using acquisition class from brainaccess.utils
-
-Change Bluetooth device name
-"""
 
 import matplotlib.pyplot as plt
 import matplotlib
 import time
+import os
 
 from brainaccess.utils import acquisition
 from brainaccess.core.eeg_manager import EEGManager
@@ -18,7 +12,6 @@ matplotlib.use("TKAgg", force=True)
 
 eeg = acquisition.EEG()
 
-# define electrode locations depending on your device
 halo: dict = {
     0: "Fp1",
     1: "Fp2",
@@ -37,14 +30,13 @@ cap: dict = {
  7: "O2",
 }
 
-# define device name
-device_name = "BA MINI 047"
+device_name = "BA MINI 046"
 
-# start EEG acquisition setup
+os.makedirs('./data', exist_ok=True)
+
 with EEGManager() as mgr:
     eeg.setup(mgr, device_name=device_name, cap=cap, sfreq=250)
 
-    # Start acquiring data
     eeg.start_acquisition()
     print("Acquisition started")
     time.sleep(3)
@@ -53,7 +45,6 @@ with EEGManager() as mgr:
     annotation = 1
     while time.time() - start_time < 5:
         time.sleep(1)
-        # send annotation to the device
         print(f"Sending annotation {annotation} to the device")
         eeg.annotate(str(annotation))
         annotation += 1
@@ -61,25 +52,18 @@ with EEGManager() as mgr:
     print("Preparing to plot data")
     time.sleep(2)
 
-    # get all eeg data and stop acquisition
     eeg.get_mne()
     eeg.stop_acquisition()
     mgr.disconnect()
 
-# Access MNE Raw object
 mne_raw = eeg.data.mne_raw
 print(f"MNE Raw object: {mne_raw}")
 
-# Access data as NumPy arrays
 data, times = mne_raw.get_data(return_times=True)
 print(f"Data shape: {data.shape}")
 
-# save EEG data to MNE fif format
 eeg.data.save(f'./data/{time.strftime("%Y%m%d_%H%M")}-raw.fif')
-# Close brainaccess library
 eeg.close()
-# conversion to microvolts
 mne_raw.apply_function(lambda x: x*10**-6)
-# Show recorded data
 mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
 plt.show()
